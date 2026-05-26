0cf4beaab5dc2b894bf491460c87d6ba28a436e1
naokton <march.of.rabbits@gmail.com>
2026-05-25 22:06:55 -0400

    Cache all available stream fields from Strava

--- src/rando_recap/strava.py
+++ src/rando_recap/strava.py
@@ -34,9 +34,13 @@ DEFAULT_STREAM_TYPES = (
     "distance",
     "latlng",
     "altitude",
+    "velocity_smooth",
     "heartrate",
     "cadence",
     "watts",
+    "temp",
+    "moving",
+    "grade_smooth",
 )
 
 
